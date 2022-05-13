21dddf9300319bd9efb423a1ce536b682f91f7dd
lvbohua <abei0518@yeah.net>
2022-05-13 13:31:22 +0800

    首页大致样式及分模块

---
+++ src/views/popup-right/measuring-tools.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="measuring-tools">
+    measuring-tools
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MeasuringTools'
+}
+</script>
+
+<style scoped lang="scss">
+.measuring-tools{
+  width: 400px;
+  height: 100%;
+  background: rgba(#141726,0.7);
+  color: blue;
+  border-left:1px solid rgba(#C0C8FF,0.4);
+}
+</style>
---
+++ src/views/popup-right/resource-management.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="resource-management">
+    resource-management
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResourceManagement'
+}
+</script>
+
+<style scoped lang="scss">
+.resource-management{
+  width: 400px;
+  height: 100%;
+  background: rgba(#141726,0.7);
+  color: #BCCCFF;
+  border-left:1px solid rgba(#C0C8FF,0.4);
+}
+</style>
