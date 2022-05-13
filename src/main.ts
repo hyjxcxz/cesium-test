71fb2b041b8d2f834b1aa7da215c6cbf7c8fbc7e
huyinjie <huyinjie@goldwind.com.cn>
2022-05-13 10:25:47 +0800

    revert: 回滚

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementPlus from 'element-plus'
-import 'element-plus/theme-chalk/index.css'
-
 
 const app = createApp(App)
-app.use(router).use(store).use(ElementPlus).mount('#app')
+app.use(router).use(store).mount('#app')
