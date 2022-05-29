35405a5b6eae846589ea4bd309e256b243460005
huyinjie <huyinjie@goldwind.com.cn>
2022-05-29 13:41:34 +0800

    feat: 搜索结果

--- src/composables/readme.md
+++ src/composables/readme.md
@@ -1,3 +1,16 @@
 # 封装的组件文件
-搜索--searchComponent
+## 搜索框--searchComponent 
 src/composables/search/searchComponent.vue
+placeholder 和searchOptions为父组件传入信息
+placeholder：搜索框提示信息
+searchOptions：搜索框左侧下拉选择数组对象
+    searchOptions=[{
+        value: 'projectName',
+        label: '项目名称'
+        }, {
+        value: 'projectNumber',
+        label: '项目编号'
+        }]
+##  搜索框查询结果列表--searchListComponent
+src/composables/searchList/searchListComponent.vue
+
---
+++ src/composables/searchList/searchListComponent.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="screen-list">
+    <ul>
+      <li
+        v-for="(item,index) in searchlist"
+        :key="index"
+        @click="listClick(item)"
+      >
+        <span>{{ item.name }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+<script lang="ts" >
+export default {
+  name: 'SearchListComponent',
+  props: {
+    searchlist: {
+      type: Array, default () { return [] }
+    }
+  },
+  emits: ['schear-list-click'],
+  setup (props:any, { emit }:any) {
+    // eslint-disable-next-line no-debugger
+    debugger
+    function listClick (item:Object) {
+      emit('schear-list-click', item)
+    }
+    return {
+      listClick
+    }
+  }
+
+}
+
+</script>
+<style scoped lang="scss">
+.screen-list{
+    width: 265px;
+    max-height:calc(100% - 200px) ;
+    background: #262B4D;
+    opacity: 0.8;
+    color: #ffff;
+    ul{
+        padding-inline-start:0px;
+        padding-inline-end:0px;
+        li{
+            cursor: pointer;
+            list-style-type: none;
+            height: 27px;
+            padding: 2px;
+            border: 1px solid #262B4D;
+            color: #BCCCFF;
+            font-weight: 400;
+            span{
+                font-size: 14px;
+                padding-left: 12px;
+                text-align: center;
+                font-family: PingFangSC-Regular, PingFang SC;
+                vertical-align: middle;
+            }
+            &:hover{
+                background: rgba(48, 73, 254, 0.2);
+                opacity: 0.7;
+                border: 1px solid #3049FE;
+                color: #617FFB;
+            }
+    }
+    }
+
+}
+</style>
