c0f8e3d588485477622670c96cd7c24eae26059b
huyinjie <huyinjie@goldwind.com.cn>
2022-09-13 11:16:54 +0800

    feat: 决策支持服务

--- src/views/support/guide-page.vue
+++ src/views/support/guide-page.vue
@@ -2,21 +2,32 @@
   <Header />
   <div class="content">
     <Menu
-      v-if="guidemenu.data.length>0"
+      v-if="guidemenu.data[0].children[0].id"
       :guidemenu="guidemenu.data"
       @menu-id="menuid"
     />
     <template v-if="APIname==='apiDocument'">
       <Guide-API
-        v-if="guideAPIdata.data.name"
+        v-if="guideAPIdata.data"
         :dataobj="guideAPIdata.data"
       />
     </template>
     <template v-else-if="APIname==='utilities'">
-      <Guide-API
-        v-if="utilitiesAPI.data"
-        :dataobj="utilitiesAPI"
-      />
+      <template v-if="utilitiesAPI.data">
+        <GuideUtils
+          v-if="(utilitiesAPI.data.titleList&&utilitiesAPI.data.titleList.length>0)"
+          :dataobj="utilitiesAPI.data"
+          :utilities-nmae="utilitiesNmae"
+        />
+        <GuideUtilsDown
+          v-else
+          :dataobj="utilitiesAPI.data"
+          :utilities-nmae="utilitiesNmae"
+        />
+      </template>
+      <template v-else>
+        <nodataComponentVue :data="nodata" />
+      </template>
     </template>
   </div>
 </template>
@@ -25,15 +36,21 @@ import { reactive, ref } from 'vue'
 import Header from '@/components/home/support-home-header.vue'
 import Menu from '@/components/menu/menu-component.vue'
 import GuideAPI from '@/components/right/guide-component.vue'
+import GuideUtils from '@/components/right/guide-utils-component.vue'
+import GuideUtilsDown from '@/components/right/guide-utils-down-component.vue'
 import { requestApi } from '@/utils/request-util'
+import nodataComponentVue from '@/composables/nodata/nodata-component.vue'
 const guidemenu = reactive({ data: [{ children: [{ id: '' }] }] })
-const guideAPIdata = reactive({ data: { returnList: [], name: '' } })
-const utilitiesAPI = reactive({ data: {} })
+const guideAPIdata = reactive({ data: { returnList: [] } })
+const utilitiesAPI = reactive({ data: { titleList: [] } })
+// const utilitiesDownload = reactive({ data: {} })
+const nodata = ref('暂无数据')
 const APIname = ref('apiDocument')
 const apiID = ref('')
+const utilitiesNmae = ref('')
 requestApi(
   '',
-  'developGuide',
+  'analysisdevelopGuide',
   null,
   (res:any) => {
     if (res.message === 'OK' && res.data) {
@@ -42,8 +59,7 @@ requestApi(
       queryAPi()
     }
   }, null)
-function dealreturnList <Y> (data:Y) :Y {
-  // const datalist = JSON.parse(JSON.stringify(data))
+function dealreturnList <T> (data:T):T {
   const datalist = data
   getchildren(datalist)
   function getchildren (arry:any) {
@@ -67,18 +83,39 @@ function menuid (obj:any) {
       break
   }
   apiID.value = obj.id
+  getMenuName()
   queryAPi()
+}
+function getMenuName () {
+  getMenue(guidemenu.data)
+  function getMenue (arry:any) {
+    arry.forEach((item: any) => {
+      if (item.id === apiID.value) {
+        utilitiesNmae.value = item.title
+      } else {
+        if (item.children) {
+          getMenue(item.children)
+        }
+      }
+    })
+  }
 }
 function queryAPi () {
   requestApi(
     '',
-    APIname.value,
+    'analysis' + APIname.value,
     null,
     (res:any) => {
       if (res.message === 'OK' && res.data) {
         if (APIname.value === 'apiDocument') {
           guideAPIdata.data = res.data
           guideAPIdata.data.returnList = dealreturnList(res.data.returnList)
+        } else if (APIname.value === 'utilities') {
+          utilitiesAPI.data = res.data
+        }
+      } else {
+        if (APIname.value === 'apiDocument') {
+          guideAPIdata.data = res.data
         } else if (APIname.value === 'utilities') {
           utilitiesAPI.data = res.data
         }
