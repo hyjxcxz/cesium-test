0e48ad8ccf8feafa61df278c384817e7a7b787a7
huyinjie <huyinjie@goldwind.com.cn>
2022-09-19 16:08:59 +0800

    feat: 修改关于

--- src/views/basedata/about-page.vue
+++ src/views/basedata/about-page.vue
@@ -2,7 +2,7 @@
   <Header />
   <div class="content">
     <iframe
-      src="src/assets/html/baseserver.html"
+      src="/html/baseserver.html"
       frameborder="0"
       style="width: 100%; height: 100%"
       id="iframeBox"
--- src/views/support/about-page.vue
+++ src/views/support/about-page.vue
@@ -2,7 +2,7 @@
   <Header />
   <div class="content">
     <iframe
-      src="src/assets/html/supportserver.html"
+      src="/html/supportserver.html"
       frameborder="0"
       style="width: 100%; height: 100%"
       id="iframeBox"
