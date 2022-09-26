e9f263f74df265a12310adcdff982419c581f7d7
huyinjie <huyinjie@goldwind.com.cn>
2022-09-26 13:25:53 +0800

    feat: 地图服务制图规范

--- src/views/mapserver/guide-page.vue
+++ src/views/mapserver/guide-page.vue
@@ -14,6 +14,13 @@
         :api-name-string="'您现在的位置：地图服务,开发指南,'+apiNameString"
       />
     </template>
+    <template v-else-if="APIname==='mapguide'">
+      <GuideUtilsDown
+        :dataobj="utilitiesAPI.data"
+        :utilities-nmae="utilitiesNmae"
+        :api-name-string="'您现在的位置：地图服务,开发指南,'+apiNameString"
+      />
+    </template>
     <template v-else>
       <nodataComponentVue :data="nodata" />
     </template>
@@ -25,12 +32,18 @@ import Header from '@/components/home/mapserver-header.vue'
 import Menu from '@/components/menu/menu-component.vue'
 import GuideAPI from '@/components/right/guide-map-component.vue'
 // import GuideUtils from '@/components/right/guide-utils-component.vue'
-// import GuideUtilsDown from '@/components/right/guide-utils-down-component.vue'
+import GuideUtilsDown from '@/components/right/guide-utils-down-component.vue'
 import { requestApi } from '@/utils/request-util'
 import nodataComponentVue from '@/composables/nodata/nodata-component.vue'
-const guidemenu = reactive({ data: [{ title: '', children: [{ title: '', id: '' }] }] })
+const guidemenu = reactive({ data: [{ title: '', id: '', children: [{ title: '', id: '' }] }] })
 const guideAPIdata = reactive({ data: { } })
-// const utilitiesAPI = reactive({ data: { titleList: [] } })
+const utilitiesAPI = reactive({
+  data: [{
+    description: '金风能源地理信息规范，通过标准化地理信息语言和系统化的标识符号，将金风科技地理信息的服务宗旨、行为和功能传递广大用户。突出地理信息标准化服务功能，有效增强用户产品体验，强化了用户心智，提升品牌形象。',
+    name: '县编码表',
+    url: './vendors/金风能源地理信息制图规范V1发布版.pdf'
+  }]
+})
 // const utilitiesDownload = reactive({ data: {} })
 const nodata = ref('暂无数据')
 const APIname = ref('apiDocument')
@@ -46,6 +59,16 @@ requestApi(
   (res:any) => {
     if (res.message === 'OK' && res.data) {
       guidemenu.data = dealreturnList(res.data)
+      // eslint-disable-next-line no-debugger
+      debugger
+      guidemenu.data.push({
+        id: 'mapguidea',
+        title: '地图规范',
+        children:
+         [
+           { id: 'mapguide', title: '制图规范' }
+         ]
+      })
       apiID.value = guidemenu.data[0].children[0].id
       apiNameString.value = guidemenu.data[0].title + ',' + guidemenu.data[0].children[0].title
       queryAPi()
@@ -66,9 +89,14 @@ function dealreturnList <T> (data:T):T {
   return datalist
 }
 function menuid (obj:any) {
+  // eslint-disable-next-line no-debugger
+  debugger
   switch (obj.parent) {
     case 'apiDocument':
       APIname.value = 'apiDocument'
+      break
+    case 'mapguidea':
+      APIname.value = 'mapguide'
       break
   }
   apiID.value = obj.id
@@ -84,6 +112,8 @@ function getMenuName () {
         apiNameString.value = item.title + ','
       }
       if (item.id === apiID.value) {
+        // eslint-disable-next-line no-debugger
+        debugger
         apiNameString.value = apiNameString.value + item.title
         utilitiesNmae.value = item.title
       } else {
